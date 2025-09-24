757fb0cd644f89b0dba351c235a8092117c63d83
LucasHor4 <lucash.arcanjo267@gmail.com>
2025-09-24 08:39:27 -0300

    Iniciando programação da tela catalogo.

---
+++ Sonatto/Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sonatto.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Catalogo()
+        {
+            return View();
+        }
+    }
+}
--- Sonatto/Models/Produto.cs
+++ Sonatto/Models/Produto.cs
@@ -2,5 +2,10 @@
 {
     public class Produto
     {
+        public required int IdProduto { get; set; }
+        public required string NomeProduto { get; set; }
+        public required decimal Preco {  get; set; }
+        
+        public required string ImagemUrl { get; set; }
     }
 }
