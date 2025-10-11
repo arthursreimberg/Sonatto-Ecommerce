52d9b7cf84960f1b950530f26a768b32566bf08e
Samuel Vasconcelos <samporvas@gmail.com>
2025-10-11 17:35:45 -0300

    Adicionando a Controller Produto e estilizando a pág de produtos

---
+++ Sonatto/Controllers/ProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sonatto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+        public IActionResult Produtos()
+        {
+            return View();
+        }
+    }
+}
