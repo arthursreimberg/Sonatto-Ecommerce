902f4e456424678fc7af240d6d9cd18361f04d17
Lucas <lucash.arcanjo267@gmail.com>
2025-10-16 19:08:25 -0300

    Arrumando tela inicial.

--- Sonatto/Program.cs
+++ Sonatto/Program.cs
@@ -44,6 +44,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Produto}/{action=Catalogo}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
